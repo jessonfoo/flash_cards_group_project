33f7d17b9e8f7b8c792a630a89cf6d314d1e77c2
Tali Herzka <taliherzka@gmail.com>
2014-08-28 12:06:53 -0700

    gameplay controller attempt

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,16 @@
 get '/'
 
 end
+
+
+get ':user_id/:deck_id' do
+  @deck = Deck.find(params[:deck_id])
+  @card = Card.find(params[:card_id])
+  erb :gameplay
+end
+
+post ':user_id/:deck_id' do
+  @deck = Deck.find(params[:deck_id])
+  @card = Card.find(params[:card_id])
+  redirect back
+end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,12 @@
 class Deck < ActiveRecord::Base
   has_many :cards
+
+  def current_card
+    current_card = self.cards.last
+  end
+
+  def next_card
+    self.cards.pop 
+  end
+
 end
