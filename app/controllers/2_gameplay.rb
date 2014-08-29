8a1b46321f80c667831bb7f43feb01a0ddfd41b5
NokoGary 3 JF <ghammell1@gmail.com>
2014-08-29 15:56:11 -0700

    I have no idea what gary has been doing. Game logic shit.

--- app/controllers/2_gameplay.rb
+++ app/controllers/2_gameplay.rb
@@ -5,11 +5,13 @@ post '/:user_id/:deck_id/:card_no' do
   @cards = @deck.cards
   @answer = params[:answer]
   @card_no = params[:card_no].to_i
+  @game_id = session[:game_id]
+  @user = params[:user_id]
 
   if @card_no > @cards.length - 1
     session[:game_id] = nil
     session[:answer] = nil
-    redirect to("/#{params[:user_id]}")
+    redirect to("/#{params[:user_id]}/#{params[:deck_id]}/game_end/#{@game_id}")
   end
 
   session[:answer] = @answer
@@ -17,11 +19,15 @@ post '/:user_id/:deck_id/:card_no' do
   redirect to("/#{params[:user_id]}/#{@deck.id}/#{@card_no}")
 end
 
+#-----------------------------------------------------
+
 get '/:user_id/:deck_id/:card_no' do
   @deck = Deck.find(params[:deck_id])
   @cards = @deck.cards
   @card_no = params[:card_no].to_i
   @current_card = @cards[@card_no]
+  @user = params[:user_id]
+
 
   if @card_no.to_i == 0
     @game = Game.create(deck_id: @deck.id, user_id: params[:user_id])
@@ -40,4 +46,13 @@ get '/:user_id/:deck_id/:card_no' do
 
   @score = game.score
   erb :gameplay
+end
+
+#----------------------------------------------------------
+
+get '/:user_id/:deck_id/game_end/:game_id' do
+  @user = User.find(params[:user_id])
+  @deck = Deck.find(params[:deck_id])
+  @game = Game.find(params[:game_id])
+  erb :game_end
 end
--- db/migrate/20140829125039_add_score.rb
+++
@@ -1,5 +0,0 @@
-class AddScore < ActiveRecord::Migration
-  def change
-    add_column :games, :score, :integer, default: 0
-  end
-end
