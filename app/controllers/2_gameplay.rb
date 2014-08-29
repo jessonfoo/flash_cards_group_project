c2b78e7b5b9339f9d3f44879cb13840491af482d
NokoGary 3 JF <ghammell1@gmail.com>
2014-08-29 14:52:18 -0700

    update * score changes on first click * redirects on second click

--- app/controllers/2_gameplay.rb
+++ app/controllers/2_gameplay.rb
@@ -8,40 +8,36 @@ post '/:user_id/:deck_id/:card_no' do
 
   if @card_no > @cards.length - 1
     session[:game_id] = nil
+    session[:answer] = nil
     redirect to("/#{params[:user_id]}")
   end
 
-  puts "SESSION SCORE: #{@score}"
+  session[:answer] = @answer
 
   redirect to("/#{params[:user_id]}/#{@deck.id}/#{@card_no}")
 end
-
-#set route from deck (from index.erb) to have card_no 0
-
 
 get '/:user_id/:deck_id/:card_no' do
   @deck = Deck.find(params[:deck_id])
   @cards = @deck.cards
   @card_no = params[:card_no].to_i
-  @answer = params[:answer]
+  @current_card = @cards[@card_no]
 
   if @card_no.to_i == 0
-    @game = Game.create(deck_id: @deck.id, user_id: params[:user_id], score: 0)
+    @game = Game.create(deck_id: @deck.id, user_id: params[:user_id])
     session[:game_id] = @game.id
   end
 
-  @current_card = @cards[@card_no]
+  game = Game.find(session[:game_id])
 
-  if @answer == @current_card.term
-    Game.find(session[:game_id]).increment(:score)
+  if session[:answer] == @current_card.term
+    game.increment(:score)
+    game.save!
     p "CORRECT ANSWER"
-    Game.save
     @card_no += 1
-
-  else
-    @card_no += 0
+    session[:correct] = true
   end
 
-  @score = Game.find(session[:game_id]).score
+  @score = game.score
   erb :gameplay
 end
